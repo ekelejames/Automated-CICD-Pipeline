f434c53fa123335ac5040289f68df9bd460f0ebe
ekelejames <ekeleenenchejames@gmail.com>
2023-12-21 18:38:04 +0100

    adding files to repo

--- plugins/bootstrap5-api/js/theme-support.js
+++ plugins/bootstrap5-api/js/theme-support.js
@@ -1,10 +1,3 @@
-if (window.getThemeManagerProperty) {
-    const setBsTheme = () => document.body.dataset['bsTheme'] = getThemeManagerProperty('bootstrap', 'theme');
-    setBsTheme();
-
-    if (window.isSystemRespectingTheme) {
-        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
-            setBsTheme()
-        });
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:1479304d41a249be0f0ba4abb69db8fbabed238b81abbfaefbcb8ed7132386c5
+size 350
--- plugins/docker-plugin/js/docker-manage.js
+++ plugins/docker-plugin/js/docker-manage.js
@@ -1,10 +1,3 @@
-
-function stop(theId) {
-    var input = document.getElementById('stopId');
-    if (input != null) {
-        input.value = theId;
-    }
-
-    var form = document.getElementById('control');
-    form.submit();
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:d2c8b5ca73084222047b176e74b00e646341cde5ec8e70c14e34849d15cee510
+size 208
